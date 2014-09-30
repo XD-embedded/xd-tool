30260d111340150a8eb679274fcf4d82918b123d
Esben Haabendal <esben@haabendal.dk>
2014-09-30 23:33:14 +0200

    Unittest coverage of init command and xd.main
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,13 +1,14 @@
 from xd.tool.main import main
-
 from nose.tools import raises, with_setup
-
 import logging
+from redirect import stdchannel_redirected
 
 
-def test_init_no_args():
+def test_main_no_args():
     main(['xd'])
 
-def test_init_early_args():
-    main(['xd', '-d'])
+
+def test_main_early_args():
+    with stdchannel_redirected(2):
+        main(['xd', '-d'])
 
