69a69b0e959128f1658b5b31b87c6a65bbb7638e
Esben Haabendal <esben@haabendal.dk>
2014-10-01 21:36:41 +0200

    Add unittest coverage of init command error handling
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>

--- tests/cmd_init_test.py
+++ tests/cmd_init_test.py
@@ -3,7 +3,9 @@ import xd.tool.log
 
 from nose.tools import raises, with_setup
 import unittest
+import sys
 import os
+import stat
 import tempfile
 import shutil
 from redirect import stdchannel_redirected
@@ -22,7 +24,7 @@ class tests(unittest.case.TestCase):
         os.chdir(self.oldcwd)
         if self.oldpwd:
             os.environ['PWD'] = self.oldpwd
-        shutil.rmtree(self.testdir)
+        shutil.rmtree(self.testdir, ignore_errors=True)
         xd.tool.log.deinit()
 
     def test_init(self):
@@ -41,7 +43,26 @@ class tests(unittest.case.TestCase):
         self.assertTrue(os.path.isdir('.git'))
         self.assertTrue(os.path.isfile('.xd'))
 
-    def test_init_fail(self):
+    def test_init_fail_1(self):
         with open('.git', 'w') as f:
             f.write('foobar')
+        with stdchannel_redirected(2):
+            self.assertNotEqual(main(['xd', 'init']), None)
+
+    def test_init_fail_2(self):
+        with open('.git', 'w') as f:
+            f.write('foobar')
+        with open('.xd', 'w') as f:
+            f.write('foobar')
         self.assertNotEqual(main(['xd', 'init']), None)
+
+    def test_init_fail_3(self):
+        os.chmod('.', stat.S_IREAD|stat.S_IEXEC)
+        with stdchannel_redirected(2):
+            self.assertNotEqual(main(['xd', 'init']), None)
+
+    def test_init_fail_4(self):
+        xd.tool.shell.call('git init', quiet=True)
+        os.chmod('.git/refs/heads', stat.S_IREAD)
+        with stdchannel_redirected(2):
+            self.assertNotEqual(main(['xd', 'init']), None)
