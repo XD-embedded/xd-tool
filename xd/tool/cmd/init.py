4f9fae1e152e4cfd1c7e79d92fd1640b8a281c74
Esben Haabendal <esben@haabendal.dk>
2014-10-01 20:54:02 +0200

    Commit .xd file when initializing manifest
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>

--- xd/tool/cmd/init.py
+++ xd/tool/cmd/init.py
@@ -13,13 +13,16 @@ def add_arguments(parser):
 # FIXME: add usage/description help text to subparser
 
 def run(args, manifest, env):
-    if not os.path.exists('.xd'):
-        with open('.xd', 'w') as f:
-            f.write('ABI=0\n')
-        log.debug('Wrote .xd file')
     if not os.path.exists('.git'):
         if not call('git init'):
             return 'git init failed'
+    if not os.path.exists('.xd'):
+        with open('.xd', 'w') as f:
+            f.write('ABI=0\n')
+        if not call('git add .xd'):
+            return 'git add failed'
+        if not call('git commit -m "Initial commit" -- .xd'):
+            return 'git commit failed'
     if call('git rev-parse --git-dir', quiet=True) != '.git\n':
         return 'Invalid manifest git dir'
     return
