1b090c25546104da9f747dbc15829dcd0cc136b3
Esben Haabendal <esben@haabendal.dk>
2014-10-04 13:13:04 +0200

    Allow manifest commands to provide subparser help text
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>

--- xd/tool/main.py
+++ xd/tool/main.py
@@ -1,4 +1,5 @@
 import logging
+import warnings
 import argparse
 import sys
 import os
@@ -61,7 +62,10 @@ def main(argv=sys.argv):
     commands = {}
     def add_parser(command):
         name = command.__name__.split('.')[-1]
-        parser = subparsers.add_parser(name)
+        parser_help = getattr(command, 'parser_help', None)
+        if parser_help is None:
+            warnings.warn('command %s missing help text'%(command.__name__))
+        parser = subparsers.add_parser(name, help=parser_help)
         command.add_arguments(parser)
         commands[name] = command
     # Add builtin subcommands
